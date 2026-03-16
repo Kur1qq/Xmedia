87658f62b8b6b250e37f69009147715bcbee2dfc
Kur1 <145770132+Kur1qq@users.noreply.github.com>
2026-03-16 10:42:49 +0800

    mentenancemode off

--- Xmedia/web/client/src/middleware.ts
+++ Xmedia/web/client/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // Set to true to enable maintenance mode, false to disable it
-const MAINTENANCE_MODE = true;
+const MAINTENANCE_MODE = false;
 
 export function middleware(request: NextRequest) {
     if (!MAINTENANCE_MODE) return NextResponse.next();
